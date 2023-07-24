1d7ee1b0619e1edcbd435a50d56f625813fc6a69
hahy23 <gkswntjr2329@gmail.com>
2023-07-24 19:31:53 +0900

    커밋

--- FrontToBack/src/main/java/com/application/FrontToBack/HomeController.java
+++ FrontToBack/src/main/java/com/application/FrontToBack/HomeController.java
@@ -92,7 +92,7 @@ public class HomeController {
 		}
 	}
 	
-	@Scheduled(cron = "0 10-40 23 * * *")
+	@Scheduled(cron = "0 10-30 23 * * *")
 	public void autoUpdate() throws Exception{
 		
 		logger.info(new Date() + "스케줄러 실행");
